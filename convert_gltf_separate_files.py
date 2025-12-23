c161724dd977f9f4ae728e52f018542ff5d9d475
Mirko Covizzi <mrkcvzz@gmail.com>
2025-12-23 18:35:45 +0100

    Add support for Blender Python API >= 4.2.0
    Version 4.2.0 and above of the Blender Python API
    does not support the `export_colors` input parameter for
    the `export_scene.gltf` function.
    
    Signed-off-by: Mirko Covizzi <mrkcvzz@gmail.com>

--- convert_gltf_separate_files.py
+++ convert_gltf_separate_files.py
@@ -17,12 +17,21 @@ def split_gltf(path, outpath):
         bpy.ops.object.select_all(action='DESELECT')
         x.select_set(True)
         file_path = os.path.join(outpath, "{}.gltf".format(x.name))
-        bpy.ops.export_scene.gltf(filepath         =  file_path,
-                                  use_selection    = True,
-                                  export_format    = 'GLB',
-                                  export_materials = 'NONE',
-                                  export_tangents  = True,
-                                  export_colors    = True)
+
+        # Blender Python API >= 4.2.0 does not support `export_colors` input parameter
+        if bpy.app.version[0] * 10 + bpy.app.version[1] >= 42:
+            bpy.ops.export_scene.gltf(filepath         =  file_path,
+                                      use_selection    = True,
+                                      export_format    = 'GLB',
+                                      export_materials = 'NONE',
+                                      export_tangents  = True)
+        else:
+            bpy.ops.export_scene.gltf(filepath         =  file_path,
+                                      use_selection    = True,
+                                      export_format    = 'GLB',
+                                      export_materials = 'NONE',
+                                      export_tangents  = True,
+                                      export_colors    = True)
 
 gltf_src = sys.argv[-2]
 gltf_out = sys.argv[-1]
